1eeb8ae0edcfa2d1d8dfe8ad7e05cde32db541f2
pietro <pietrof@live.com>
2021-10-16 20:13:41 +0200

    Update server.php

--- server.php
+++ server.php
@@ -3,8 +3,8 @@
   $img = str_replace('data:image/png;base64,', '', $img);
   $img = str_replace(' ', '+', $img);
   $data = base64_decode($img);
-  // $file = 'newSketches/' . uniqid() . '.png';
-  $file = 'http://pietroforino.com/img/' . uniqid() . '.png';
+  $file = 'newSketches/' . uniqid() . '.png';
+  // $file = 'http://pietroforino.com/img/' . uniqid() . '.png';
   $success = file_put_contents($file, $data);
   print $success ? $file : 'Impossibile salvare il file sul server';
 
